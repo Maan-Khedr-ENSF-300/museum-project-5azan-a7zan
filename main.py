f0ed4339bf4565c0509d9a83c254d52dff739b35
AbdelrahmanBekhit <abdelrahman.bekhit@ucalgary.ca>
2022-12-06 23:46:38 -0700

    identify_user

--- main.py
+++ main.py
@@ -33,3 +33,28 @@ def print_result():
     print("Search found ",len(search_result)," Entries:\n")
     search_result.insert(0, col_names)
     print(tabulate(search_result))
+
+def identify_user(x,y):
+    '''
+    is able to identify which user 
+    '''
+    member_list=[]
+    with open("member.csv","r") as folder:
+        reader = csv.reader(folder,delimiter=',')
+        for row in reader:
+            items = dict(member=row[0], member_pass=row[1], authentication=row[2])
+            member_list.append(items)
+    #above code creates a list with the rows of the csv file as dictionaries
+    for i in member_list:
+        if (i.get('member') == x and i.get('member_pass') == y): #iterates over the list and checks if username and pass are valid.
+            if (i.get('authentication') == 'admin'):
+                return 1,member_list
+            elif (i.get('authentication') == 'data_entry_users'):
+                return 2,0
+            elif (i.get('authentication') == 'end_user'):
+                return 3,0
+            elif (i.get('authentication') == 'blocked'):
+                return 4,0
+        else:
+            continue
+    return 0,0
