59490d4e3b92264f0a1653b149b74910198cb32e
TheOnlyYoussef <113855929+TheOnlyYoussef@users.noreply.github.com>
2022-12-06 23:29:28 -0700

    read_sql function

--- main.py
+++ main.py
@@ -13,3 +13,16 @@ def connect(user_password):
     # Get a cursor
     global cur
     cur = cnx.cursor()
+
+def read_sql(filepath):
+    fd = open(filepath, 'r')
+    sqlFile = fd.read()
+    fd.close()
+    sqlCommands = sqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            if command.strip() != '':
+                cur.execute(command)
+        except:
+            print("Command skipped: ", command)
